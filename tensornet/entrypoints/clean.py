205d2542ea75e29d5f2d3bdfbfd6b939725912fe
bigd4 <141120108@smail.nju.edu.cn>
2023-01-20 18:06:53 +0800

    fix bug in command clean

--- tensornet/entrypoints/clean.py
+++ tensornet/entrypoints/clean.py
@@ -1,11 +1,16 @@
 import os
 
 
-def clean(*args, **kwargs):
+def main(*args, **kwargs):
     i = 0
     while os.path.exists("results{}".format(i)):
         i += 1
     os.mkdir("results{}".format(i))
-    os.system("mv allpara.yaml log.txt *err* *out* input.yaml *.pt results{}".format(i))
+    os.system("mv allpara.yaml log.txt *err* *out* *.pt results{}".format(i))
+    os.system("cp input.yaml *.pt results{}".format(i))
     print("Done!")
+
+
+if __name__ == "__main__":
+    main()
 
